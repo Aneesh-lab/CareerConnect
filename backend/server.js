dd7fcba7730d46f6e6db3aa110af9daabff4f660
Aneesh-lab <aneeshmishra9098@gmail.com>
2026-02-16 17:44:04 +0530

    Created models for app

--- backend/server.js
+++ backend/server.js
@@ -2,5 +2,25 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose'; 
+import postRoutes from "./routes/posts.routes.js";
 
 dotenv.config();
+
+const app = express();
+
+app.use(cors());
+
+app.use(postRoutes);
+
+app.use(express.json());
+
+const start = async () => {
+    
+    const connectDB = await mongoose.connect(process.env.MONGO_URI);
+    
+    app.listen(9080, () => {
+console.log("Server is running on port 9080")
+    })
+
+}
+start();
